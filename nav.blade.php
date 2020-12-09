e2d869ec19c4d8c3366c31f118a027df377fb972
sitisalbiah26 <55779782+sitisalbiah26@users.noreply.github.com>
2020-12-09 21:12:14 +0800

    Create nav.blade.php

---
+++ nav.blade.php
@@ -0,0 +1,13 @@
+<!--nav-bar-->
+<ul>
+    <li> <a href="/about"> Listing</a></li>
+</ul>
+
+@extends('master')
+
+@section('title', 'ISBN', 'Book Category', 'No of pages')
+
+@section('list')
+    <h1>Ebook Sharing Application</h1>
+    <p>The platform is easy to find the ebook</p>
+@endsection
