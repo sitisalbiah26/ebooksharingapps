6c7ef338c9aa584a33beb012e8b3aebd858afbc9
sitisalbiah26 <55779782+sitisalbiah26@users.noreply.github.com>
2020-12-09 21:13:35 +0800

    Create list.blade.php

---
+++ list.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+</head>
+<body>
+    <h1>Ebook Sharing Application</h1>
+    <h1> {{ $ebookTitle, $ebookISBN, $ebookCategory, $ebookNoOfPages }} </h1>
+
+</body>
+</html>
